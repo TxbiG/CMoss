44c07643f3c17107eda2d3a358367eb24e502fa3
Toby <126118957+TxbiG@users.noreply.github.com>
2025-09-28 21:17:24 +0100

    Update Moss_Renderer.h

--- include/Moss/Moss_Renderer.h
+++ include/Moss/Moss_Renderer.h
@@ -45,6 +45,76 @@ extern "C" {
 #include <Metal/Metal.hpp>
 #endif // MOSS_USE_METAL
 
+enum TextureFormat {
+    // Unsigned normalized color formats
+    R8, 
+    RG8, 
+    RGB8, 
+    RGBA8,
+    
+    // Signed normalized formats
+    R8_SNORM, 
+    RG8_SNORM, 
+    RGB8_SNORM, 
+    RGBA8_SNORM,
+    
+    // Floating-point formats
+    R16F, 
+    RG16F, 
+    RGB16F, 
+    RGBA16F,
+    R32F, 
+    RG32F, 
+    RGB32F, 
+    RGBA32F,
+    
+    // Integer formats
+    R8UI, 
+    RG8UI, 
+    RGBA8UI, 
+    R16UI, 
+    RG16UI,
+    RGBA16UI, 
+    R32UI, 
+    RG32UI, 
+    RGBA32UI,
+    
+    // Depth formats
+    Depth16, 
+    Depth24, 
+    Depth32F, 
+    Depth24Stencil8, 
+    Depth32FStencil8,
+    
+    // Compressed (optional support)
+    DXT1, 
+    DXT3, 
+    DXT5, 
+    BC4, 
+    BC5, 
+    BC6H, 
+    BC7,
+    
+    // sRGB formats
+    SRGB8, 
+    SRGBA8
+};
+
+enum InputLayoutDescription {
+		Position,						///< 3 float position
+		Color,							///< 4 uint8 color
+		Normal,							///< 3 float normal
+		TexCoord,						///< 2 float texture coordinate
+		InstanceColor,					///< 4 uint8 per instance color
+		InstanceTransform,				///< 4x4 float per instance transform
+		InstanceInvTransform,			///< 4x4 float per instance inverse transform
+	};
+enum class DrawPass { Shadow = 0U, Normal = 1U, Lighting = 2U, Transparent = 3U }; // In which draw pass to use this pipeline state
+enum class Topology { Triangle = 0U, Line = 1U, Point = 2U };					            // The type of topology to emit
+enum class FillMode { Solid = 0U, Wireframe = 1U };						                    // Fill mode of the triangles (<-Should be in Renderer Settings)
+enum class DepthTest { Off = 0U, On = 1U };								                        // If depth write / depth test is on
+enum class BlendMode { Opaque = 0U, Alpha = 1U, Additive = 2U };			              // How to blend the pixel from the shader in the back buffer
+enum class CullMode { None = 0U, Backface = 1U, FrontFace = 2U };				          // How to cull triangles
 
 struct Moss_Renderer;
 struct Camera2D;
@@ -74,6 +144,23 @@ MOSS_API void Moss_RendererPresentFrame(Moss_Renderer* renderer);
 /*! @brief Free renderer. @param Moss_Renderer renderer. @ingroup Renderer. */
 MOSS_API void Moss_TerminateRenderer(Moss_Renderer* renderer);
 
+MOSS_API Camera2* Moss_CreateCamera2(Moss_Renderer* renderer, );
+MOSS_API void Moss_DestoryCamera2(Moss_Renderer* renderer, );
+
+MOSS_API Camera3* Moss_CreateCamera3(Moss_Renderer* renderer, );
+MOSS_API void  Moss_DestoryCamera3(Moss_Renderer* renderer, );
+
+MOSS_API Moss_CreatePipelinestate(Moss_Renderer* renderer, VertexShader* vertex, const InputDescription* inputDescription, uint inputDescriptionCount, 
+      const PixelShaderVK* pixel, DrawPass drawPass, FillMode fillMode, Topology topology, 
+      EDepthTest inDepthTest, EBlendMode inBlendMode, ECullMode inCullMode);
+
+MOSS_API Moss_DestoryPipelinestate(Moss_Renderer* renderer, );
+
+MOSS_API Moss_CreateTexture(Moss_Renderer* renderer, );
+MOSS_API void Moss_DestoryTexture(Moss_Renderer* renderer, );
+
+MOSS_API Moss_CreateSurface(Moss_Renderer* renderer, );
+MOSS_API void Moss_DestorySurface(Moss_Renderer* renderer, );
 
 // Graphics Specific
 #ifdef MOSS_GRAPHICS_OPENGL
@@ -94,6 +181,7 @@ MOSS_API void Moss_TerminateRenderer(Moss_Renderer* renderer);
 
 
 #endif // MOSS_RENDERER_H
+
 
 
 
